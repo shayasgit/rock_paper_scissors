8cd920762af9bcbabc06877838a373fad7336d17
Shayas R.A <shayas.contact@gmail.com>
2024-04-19 18:27:57 +0300

    Both the Player and Computer Choices are created
    Created getPlayerChoice and getComputerChoice fn
    are made to assign player and computer Selection

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
 console.log('Rock Paper Scissors');
+
+let playerSelection = getPlayerSelection();
+let computerSelection = getComputerChoice();
+
+function getComputerChoice() {
+    let choice = ['ROCK', 'PAPER', 'SCISSORS'];
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+function getPlayerSelection() {
+    let choice = prompt("Enter player choice from 'ROCK | PAPER | SCISSORS", "");
+    if(choice == '' || choice == null) {
+        alert("Game cancelled by player")
+        return 'CANCELLED';
+    } 
+    choice = choice.toLocaleUpperCase();
+    if (choice == 'ROCK' || choice == 'PAPER' || choice == 'SCISSORS') {
+        return choice;
+    } else {
+        alert("Enter from given choices");
+        return getPlayerSelection();
+    }
+}
+
+console.log(playerSelection);
+console.log(computerSelection);
