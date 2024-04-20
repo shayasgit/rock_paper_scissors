036d43ba807a443d2e7fd92461ae876c3c9cf3b7
Shayas R.A <shayas.contact@gmail.com>
2024-04-20 09:21:37 +0300

    playRound fn created
    To play game against player and computer
    Rock Paper Scissors

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
 
 function getPlayerSelection() {
     let choice = prompt("Enter player choice from 'ROCK | PAPER | SCISSORS", "");
-    if(choice == '' || choice == null) {
+    if(choice == null) {
         alert("Game cancelled by player")
         return 'CANCELLED';
     } 
@@ -25,3 +25,25 @@ function getPlayerSelection() {
 
 console.log(playerSelection);
 console.log(computerSelection);
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return `It's TIE!`
+    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
+        return "You Loose! Paper beats Rock";
+    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
+        return `You Win! Rock beats Scissors`;
+    } else if (playerSelection == 'PAPER' && computerSelection == "ROCK") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
+        return "You Lose! Scissor beats Paper";
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+        return "You Loose! Rock beats Scissors";
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return 'Not Defined'
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection));
