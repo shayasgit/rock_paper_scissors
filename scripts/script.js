92f3ec3c5a2dbc959b8da3307c356e8384683c6a
Shayas R.A <shayas.contact@gmail.com>
2024-04-20 09:49:29 +0300

    Fn for playGame Created and declare winner
    playGame fn created and looped 5 times and keep score
    and declare winner at the end

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 console.log('Rock Paper Scissors');
 
-let playerSelection = getPlayerSelection();
-let computerSelection = getComputerChoice();
-
 function getComputerChoice() {
     let choice = ['ROCK', 'PAPER', 'SCISSORS'];
     return choice[Math.floor(Math.random() * choice.length)];
@@ -10,10 +7,10 @@ function getComputerChoice() {
 
 function getPlayerSelection() {
     let choice = prompt("Enter player choice from 'ROCK | PAPER | SCISSORS", "");
-    if(choice == null) {
+    if (choice == null) {
         alert("Game cancelled by player")
         return 'CANCELLED';
-    } 
+    }
     choice = choice.toLocaleUpperCase();
     if (choice == 'ROCK' || choice == 'PAPER' || choice == 'SCISSORS') {
         return choice;
@@ -23,27 +20,63 @@ function getPlayerSelection() {
     }
 }
 
-console.log(playerSelection);
-console.log(computerSelection);
 
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
+        playerScore += 0;
+        computerScore += 0;
         return `It's TIE!`
     } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
+        playerScore += 0;
+        computerScore += 1;
         return "You Loose! Paper beats Rock";
     } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
+        playerScore += 1;
+        computerScore += 0;
         return `You Win! Rock beats Scissors`;
     } else if (playerSelection == 'PAPER' && computerSelection == "ROCK") {
+        playerScore += 1;
+        computerScore += 0;
         return "You Win! Paper beats Rock";
     } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
+        playerScore += 0;
+        computerScore += 1;
         return "You Lose! Scissor beats Paper";
     } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+        playerScore += 0;
+        computerScore += 1;
         return "You Loose! Rock beats Scissors";
     } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
+        playerScore += 1;
+        computerScore += 0;
         return "You Win! Scissors beats Paper";
     } else {
-        return 'Not Defined'
+        return 'Game Cancelled by the player'
     }
 }
 
-console.log(playRound(playerSelection,computerSelection));
+function playGame() {
+    let playerSelection = getPlayerSelection();
+    let computerSelection = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    return playRound(playerSelection, computerSelection)
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i = 0; i < 5; i++) {
+    console.log(playGame());
+}
+
+console.log("Player Score is " + playerScore);
+console.log("Computer Score is " + computerScore);
+
+if (playerScore > computerScore) {
+    console.log(`Winner is Player`);
+} else if (playerScore < computerScore) {
+    console.log(`Winner is Computer`)
+} else {
+    console.log(`It's TIE!`);
+}
